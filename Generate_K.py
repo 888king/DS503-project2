9ed35a7b5d1f6f8ff4d9ccbeeaf6c754e8e7049a
888king <yzhai2@wpi.edu>
2020-02-21 21:57:58 -0500

    update

---
+++ Generate_K.py
@@ -0,0 +1,16 @@
+import random
+import pandas as pd
+
+
+x = []
+y = []
+
+for i in range(1000):
+    x.append(random.randint(1, 10000))
+    y.append(random.randint(1, 10000))
+
+records = pd.DataFrame({
+    'x': x,
+    'y': y
+})
+records.to_csv('initial.csv', index=None)
